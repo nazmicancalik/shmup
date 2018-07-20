368d7c40489bc57f085ad0559b753f8501ffb7ca
nazmicancalik <nazmicancalik@gmail.com>
2018-07-20 20:04:27 +0200

    Meteor rotation

--- shmup.py
+++ shmup.py
@@ -61,18 +61,23 @@ class Player(pg.sprite.Sprite):
 class Mob(pg.sprite.Sprite):
     def __init__(self):
         pg.sprite.Sprite.__init__(self)
-        self.image = pg.transform.scale(random.choice(meteors), (50, 48))
-        self.image.set_colorkey(BLACK)
+        self.image_orig = pg.transform.scale(random.choice(meteors), (50, 48))
+        self.image_orig.set_colorkey(BLACK)
+        self.image = self.image_orig.copy()
         self.rect = self.image.get_rect()
         self.radius = int(self.rect.width * .85 / 2)
         self.rect.x = random.randint(0, WIDTH - self.rect.width)
         self.rect.bottom = random.randint(-100, -40)
         self.speedy = random.randint(2, 5)
         self.speedx = random.randint(4, 5)
+        self.rot = 0
+        self.rot_speed = random.randint(-8, 8)
+        self.last_update = pg.time.get_ticks()
 
     def update(self):
         self.rect.y += self.speedy
         self.rect.x += self.speedx
+        self.rotate()
 
         # Going to bottom spawns upside
         if self.rect.top > HEIGHT + 10:
@@ -83,6 +88,17 @@ class Mob(pg.sprite.Sprite):
         # Make the mobs jump back from sides
         if self.rect.left < 0 or self.rect.right > WIDTH:
             self.speedx = -1 * self.speedx
+
+    def rotate(self):
+        now = pg.time.get_ticks()
+        if now - self.last_update > 50:
+            self.last_update = now
+            self.rot = (self.rot + self.rot_speed) % 360
+            new_image = pg.transform.rotate(self.image_orig, self.rot)
+            old_center = self.rect.center
+            self.image = new_image
+            self.rect = self.image.get_rect()
+            self.rect.center = old_center
 
 
 class Bullet(pg.sprite.Sprite):
